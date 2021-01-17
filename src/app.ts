2fba10fc00ebb5f14868e23b21d9733b6ae48de7
Galileo <Galileo-dev@users.noreply.github.com>
2021-01-17 15:54:07 +0000

    Added .env and example

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 //Due to Hormony error run with --no-check flag
 // deno run --no-check --allow-net index.ts
 // Add to server using https://discord.com/oauth2/authorize?client_id=800345465828409365&scope=bot
+// Glitch deno dir: /app/.deno/bin/deno
+// Glitch denon dir: /app/.deno/bin/denon
 
 import { Client, Intents, Message } from "https://deno.land/x/harmony/mod.ts";
-import Token from "./token.ts";
+import "https://deno.land/x/dotenv/load.ts";
 
 const client = new Client();
-client.connect(Token, Intents.None);
+client.connect(Deno.env.get("BOTTOKEN"), Intents.None);
 
 client.on("ready", () => {
   console.log("ready");
